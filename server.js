81670b112f1915096cd357aa8c167b868d3014a8
wuzzlefump <paulelliottwork@gmail.com>
2020-09-03 22:49:21 -0600

    API path

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ let PORT = process.env.PORT || 3000;
 // set up middle
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"))
 
 
 //functionality
@@ -20,10 +21,21 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
   
+
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
 
+
+app.get("/api/notes", function(req,res){
+
+  return fs.readFile(__dirname + "/db/db.json", 'utf8', function(err, data) {
+    if (err) throw err;
+    obj =JSON.parse(data)
+    res.json(obj);
+  });
+
+});
 
 
 
